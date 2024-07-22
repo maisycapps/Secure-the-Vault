27923cfea271f03d21fd476dc157fc82fd3a152d
Maisy Capps <maisycapps@gmail.com>
2024-07-22 10:35:51 -0700

    vault commit

---
+++ vault_script.js
@@ -0,0 +1,25 @@
+/* STEPS:
+mathematical calculation 1 = variable 1 (10)
+mathematical calculation 2 = variable 2 (40)
+mathematical calculation 3 = variable 3 (39)
+alert(`${userMessage} ${variable 1} - ${variable 2} - ${variable 3}`)
+*/ 
+
+// user message
+let userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// math for code 1
+let variable1 = 5 + 5;
+
+// math for code 2
+let variable2 = 2 * 20;
+
+// math for code 3
+let variable3 = 50 - 11; 
+
+// string for alert
+let codeMessage = `${userMessage} ${variable1}-${variable2}-${variable3}`
+
+// alert
+alert(codeMessage);
+
